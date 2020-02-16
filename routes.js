c56829cfe6614037119770d31c267cfe463e3a9e
Michael Kolber <michaelkolber@users.noreply.github.com>
2020-02-16 09:54:14 -0500

    Clean up formatting, add express

--- routes.js
+++ routes.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.route('/') // handling a request
+app.route('/')
 	.get(function (req, res) {
 		res.render('index.html');
-	})
+	});
 
 app.route('/login')
 	.get(function (req, res) {
-		res.render('loginPage.html', { Error Message: "no login" });
+		res.render('loginPage.html', { ErrorMessage: "no login" });
 	})
 	.post(function (req, res) {
 		let username = req.body.username;
@@ -22,93 +22,92 @@ app.route('/login')
 		} else {
 			res.send('Invalid login')
 		}
-	})
+	});
 
 app.route('/signup')
-	.get(function (req, res)){
-	res.render('signUp.html');
-
-})
-	.post(function (req, res)){
-	let username = req.body.username;
-	let password = req.body.password;
-
-	createAccount(username, password);
-		res.redirect('/emailVerification');
-}
+	.get(function (req, res){
+		res.render('signUp.html');
 	})
+	.post(function (req, res){
+		let username = req.body.username;
+		let password = req.body.password;
+		
+		createAccount(username, password);
+			res.redirect('/emailVerification');
+	});
+
 app.route('/emailVerification')
-	.get(function (req, res)) {
-	render('verifyEmail.html');
-}
-	.post(function (req, res)) {
-	let username = req.body.username;
-	let vCode = req.body.verificationCode;
-	if (isCorrect(username, vCode)) {
+	.get(function (req, res) {
+		render('verifyEmail.html');
+	})
+	.post(function (req, res) {
+		let username = req.body.username;
+		let vCode = req.body.verificationCode;
+		if (isCorrect(username, vCode)) {
+			let university = res.locals.domain;
+			app.session.university = university;
+			res.redirect('/' + university);
+		}
+	});
+
+app.route('/api/v1/university')
+	.post(function (req, res) {
+		createUniversity(req.body.universityId, req.body.universityName)
 		let university = res.locals.domain;
 		app.session.university = university;
 		res.redirect('/' + university);
-	}
-}
-
-app.route('/api/v1/university')
-	.post(function (req, res)) {
-	createUniversity(req.body.universityId, req.body.universityName)
-	let university = res.locals.domain;
-	app.session.university = university;
-	res.redirect('/' + university);
-}
+	});
 
 app.route('/api/v1/university/chat') // for deleting the chat
 
 app.route('/:university')
-	.get(function (res, req)) {
-	if (!universityExists(req.locals.domain)) {
-		res.render('createUniversity.html', { university: req.session.university })
-	}
-	else {
-		res.render('university.html', { university: req.session.university }
+	.get(function (res, req) {
+		if (!universityExists(req.locals.domain)) {
+			res.render('createUniversity.html', { university: req.session.university })
 		}
+		else {
+			res.render('university.html', { university: req.session.university })
+		}
+	});
 
-
-
-})
 //res.render('loginPage.html', {university: req.session.university}
 app.route('/:university/search')
-	.get((function (req, res)) {
-	res.render('courseSearch.html');
+	.get(function (req, res) {
+		res.render('courseSearch.html');
 	})
-	.post(function (req, res)) {
-	let domain = res.locals.domain;
-	let subject = req.body.subjectName;
-	let courseNumber = req.body.courseNumber;
-	getChatsForCourse(domain, subject, courseNumber)
-	res.render('searchResults.html', {department: department, courseNumber : courseNumber})
-}
-app.route('/:university/AddChat')
-	.get((function (req, res)) {
-	res.render('addChat.html');
-	})
-	.post(function (req, res)) {
-	let username = req.session.username;
-	let subject = req.body.subject;
-	let course = req.body.course;
-	let section = req.body.section;
-	let startTime = req.body.startTime;
-	let days = req.body.days;
-	let roomNumber = req.body.roomNumber;
-	let building = req.body.building;
-	let professor = req.body.room;
-	let chatLink = req.body.chatLink;
-	let domain = res.locals.domain;
+	.post(function (req, res) {
+		let domain = res.locals.domain;
+		let subject = req.body.subjectName;
+		let courseNumber = req.body.courseNumber;
+		getChatsForCourse(domain, subject, courseNumber)
+		res.render('searchResults.html', {department: department, courseNumber : courseNumber})
+	});
 
-	addChat(subject, course, section, startTime, days, roomNumber, building, professor, chatLink, domain, username);
-	res.redirect('/:university/:subject/:courseNumber');
-}
+app.route('/:university/AddChat')
+	.get(function (req, res) {
+		res.render('addChat.html');
+	})
+	.post(function (req, res) {
+		let username = req.session.username;
+		let subject = req.body.subject;
+		let course = req.body.course;
+		let section = req.body.section;
+		let startTime = req.body.startTime;
+		let days = req.body.days;
+		let roomNumber = req.body.roomNumber;
+		let building = req.body.building;
+		let professor = req.body.room;
+		let chatLink = req.body.chatLink;
+		let domain = res.locals.domain;
+
+		addChat(subject, course, section, startTime, days, roomNumber, building, professor, chatLink, domain, username);
+		res.redirect('/:university/:subject/:courseNumber');
+	})
+
 app.route('/:university/:subject/:courseNumber')
-	.get(function (req, res)) {
-	res.render('courseChats.html');
-}
+	.get(function (req, res) {
+		res.render('courseChats.html');
+	});
 
 //write the paths and the functions associated with it
 
