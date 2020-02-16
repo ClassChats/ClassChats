de771c79a35eb5b2d40b5a4d516054b7f14503d0
ericshermancs <zensap92@gmail.com>
2020-02-16 03:29:37 -0500

    push first function

---
+++ dbFunctions.js
@@ -0,0 +1,49 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dbuser',
+  password : 'password',
+});
+
+connection.connect();
+
+
+function getChatsForCourse(domain, subject, number, callback){
+	// do the query
+	// in the callback:
+	// function(err, result)
+	domain = domain.replace('.', '_')
+	connection.query(`
+		USE ${domain};
+	`, function(err, result){
+		if(err) throw err;
+		connection.query(`
+			SELECT courseid FROM Course
+			LEFT JOIN Subject 
+			ON Course.subjectid = Subject.subjectid
+			WHERE Subject.name = '${subject}' AND Course.number = '${number}';
+			`, function(err, result) {
+				if (err) throw err;
+				let courseid = result[0]['courseid']
+				connection.query(`
+					SELECT * FROM Chats LEFT JOIN Class ON Class.courseid=${courseid}; 
+				`, function(err, result){
+					if(err) throw err;
+					callback(null, result);
+				})
+
+			})
+		//callback(null, result)
+	})
+}
+
+
+// on their side...
+/*
+getChatsForCourse("CSCI", "111", function(err, result) {
+	if (err); // panic
+	else renderResult(result);
+})
+*/
+
+getChatsForCourse('cuny.edu', 'csci', '111')
