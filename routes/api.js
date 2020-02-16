d21e10c02e24a937ba3a6880d5bc712cea17e532
Michael Kolber <michaelkolber@users.noreply.github.com>
2020-02-16 10:49:42 -0500

    Compartmentalize logic into different routes

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let router = express.Router();
+
+router.route('/universities/:domain')
+	.post(function (req, res) {
+        if (req.app.session.domain !== req.params.domain) {
+            res.json({ok: false, error: "You can't change the details of another school's domain."});
+            return;
+        }
+        
+		createUniversity(req.app.session.domain, req.body.name, function(err, result) {
+            if (err) {
+                res.json({ok: false, error: err});
+                return;
+            }
+            res.json({ok: true});
+        });
+	});
+
+app.route('/universities/:domain/chats/:chatId');
