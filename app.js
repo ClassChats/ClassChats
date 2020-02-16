27008d8a85650cf9b79d7a24342d9961e3eaea49
Michael Kolber <michaelkolber@users.noreply.github.com>
2020-02-16 12:53:04 -0500

    Add session and EJS support

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require('express-session')
+
 const app = express();
 const port = 3000;
 
@@ -6,11 +8,25 @@ const port = 3000;
 app.use(express.static('public'));
 // Properly handle JSON
 app.use(express.json());
+// Set up session management
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'best hackathon team',
+}));
+// Set the view engine
+app.set('view engine', 'ejs');
 
 // Import routers
 const baseRouter = require('./routes/base');
 const universityRouter = require('./routes/u');
 const apiRouter = require('./routes/api');
+
+// Ensure session data is available locally
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 // Set up paths
 app.use('/', baseRouter);
