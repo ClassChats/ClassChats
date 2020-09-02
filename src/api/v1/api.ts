de22e8c15a5a0ea0ed1dfdd341e741916e1bd3aa
michaelkolber <git@klbr.dev>
2020-09-02 01:28:11 -0400

    Move main server logic into src/server.ts - Now, API is loaded under path /api/v1/

--- src/api/v1/api.ts
+++ src/api/v1/api.ts
@@ -1,25 +1,18 @@
 import Fastify = require('fastify');
 import sequelize = require('../../db/setup');
 
-const fastify = Fastify({ logger: true });
+async function routes(fastify: Fastify.FastifyInstance, options) {
+    // Add a Sequelize instance to our Fastify instance
+    fastify.decorate('db', sequelize);
 
-// Add a Sequelize instance to our Fastify instance
-fastify.decorate('db', sequelize);
+    // Register routes
+    const routes = {
+        universities: require('./universities'),
+    };
 
-// Register routes
-const routes = {
-    universities: require('./universities'),
-};
+    fastify.register(routes.universities, {
+        prefix: '/universities',
+    });
+}
 
-fastify.register(routes.universities, {
-    prefix: '/universities',
-});
-
-// Run the server!
-fastify.listen(3000, '0.0.0.0', function(err, address) {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-    fastify.log.info(`server listening on ${address}`);
-});
+export = routes;
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,34 @@
-// Set up environment variables
-require('dotenv').config();
-
-// Require the framework and instantiate it
-import fastifyFactory from 'fastify';
-const fastify = fastifyFactory({ logger: true });
-
-// Route imports
-import adminRoutes = require('./routes/admin');
-import baseRoutes = require('./routes/base');
-import domainRoutes = require('./routes/domain/domain');
-
-// Other imports
+import Fastify = require('fastify');
 import { AddressInfo } from 'net';
 
+// The server instance
+const fastify = Fastify({ logger: true });
+
+// Register routes
+const routes = {
+    api: {
+        v1: require('./api/v1/api'),
+    },
+};
+
+fastify.register(routes.api.v1, {
+    prefix: '/api/v1',
+});
+
 // Redirect trailing slashes to non-trailing slashes
-fastify.addHook('onRequest', (request, reply, done) => {
-    console.log(request.req.url);
+fastify.addHook('onRequest', async (request, reply) => {
     if (request.req.url!.match(/\/.*\/+$/)) {
         // Replace empty string with '/' to avoid issues with the root path
         const newUrl = request.req.url!.replace(/\/+$/, '') === '' ? '/' : '';
         reply.redirect(301, newUrl);
     }
-
-    done();
 });
 
-// Register base and prefixed routes
-fastify.register(baseRoutes);
-fastify.register(domainRoutes, { prefix: '/:domain' });
-fastify.register(adminRoutes, { prefix: '/admin' });
-
-// Run the server!
-async function start() {
-    try {
-        await fastify.listen(3000, '0.0.0.0');
-        fastify.log.info(
-            `server listening on ${
-                (fastify.server.address() as AddressInfo).port
-            }`,
-        );
-    } catch (err) {
+// Start the server
+fastify.listen(3000, '0.0.0.0', function(err, address) {
+    if (err) {
         fastify.log.error(err);
         process.exit(1);
     }
-}
-start();
+    fastify.log.info(`Server listening at ${address}`);
+});
